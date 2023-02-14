c25da6a991df9998e72069954c74ac4790d7bee2
Mario Triana <mariotriana1978@gmail.com>
2023-02-13 22:42:10 -0500

    js type module changed

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-import { projectsList } from './data.js'
-import { services } from './data.js'
-import { skillsList } from './data.js'
-
 
 //Nav
 function select(link){
@@ -26,6 +22,36 @@ function responsiveMenu(){
         menu.className = "";
     }
 }
+const skillsList = [
+    {
+        'lang': "Django",
+        'percentage': "70%"
+    },
+    {
+        'lang': "Flask",
+        'percentage': "70%"
+    },
+    {
+        'lang': "Python",
+        'percentage': "80%"
+    },
+    {
+        'lang': "React",
+        'percentage': "60%"
+    },
+    {
+        'lang': "Java Script",
+        'percentage': "70%"
+    },
+    {
+        'lang': "CSS",
+        'percentage': "70%"
+    },
+    {
+        'lang': "HTML",
+        'percentage': "70%"
+    }
+]
 let div1 = document.getElementById("skill");
 function skill(){  
         for(let i=0;i < skillsList.length;i++){
@@ -43,6 +69,23 @@ function skill(){
     }
 // SERVICES
 
+const services = [
+    {
+        'area':"Frontend",
+        'langs':['CSS','HTML','JS'],
+        'seniority':'Junior'
+    },
+    {
+        'area':"Backend",
+        'langs':['Flask','Django','FastAPI'],
+        'seniority':'Junior'
+    },
+    {
+        'area':"Programming",
+        'langs':['','Python',''],
+        'seniority':'Junior'
+    }  
+]
 let service = document.getElementById("service");
 function servicesGetter(){     
     for(let i=0;i < services.length;i++){
@@ -63,6 +106,22 @@ function servicesGetter(){
     }
 
 // PORTFOLIO
+
+const projectsList = [
+    {
+        'name': 'QUIZ',
+        'description': 'Quiz game made in JS' ,
+        'imgLink': './assets/images/project-images/DALL·E-Quiz.png',
+        'siteLink': "https://mariopw.github.io/Quiz/"
+    },
+    {
+        'name': 'Recipe Calculator',
+        'description': "Recipe calculator made in JS",
+        'imgLink': './assets/images/project-images/DALL·E-recipe-calculator.png',
+        'siteLink': "https://mariopw.github.io/Recipe-Calculator/",
+        'repoLink': 'https://github.com/MarioPW/Quiz'
+    }
+]
 
 let divProject = document.getElementById("projects")
 
@@ -85,8 +144,6 @@ function getProject(){
         `
     }
 }
-
-
 // Email sender
 const btn = document.getElementById('button');
 
